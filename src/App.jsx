49d446f2702477f510f64a27edb384b5b6a52a4d
Saemi Park <saemiprk@gmail.com>
2024-06-17 22:48:14 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,19 @@ function App() {
   return (
     <>
     {/* main */}
-    <section className='relative w-full h-full'>
-      <img className='absolute w-full' src='images/main.jpg' />
+    <section className='relative w-full'>
+      <img className='w-full' src='images/main.jpg' />
       {/* <h1 className='absolute top-0 left-0 w-full mt-10'>우리 결혼 합니다!</h1> */}
-      <h1 className='absolute w-full mt-12 text-white drop-shadow-lg'>
+      <h1 className='absolute w-full mt-12 top-0 left-0 text-white drop-shadow-lg'>
         <Typewriter
             words={['우리, 결혼합니다!']}
             cursor = {false}
           />
       </h1>
-      <div className='main_info absolute left-0 bottom-36 w-full text-white drop-shadow-lg text-2xl'>
-        박새미 & 이태현
+      <div className='main_info absolute left-0 bottom-10 w-full text-white drop-shadow-lg text-xl'>
+        <span className='flex items-center justify-center mb-3 text-lg'>박새미 & 이태현</span>
+        <span className='flex items-center justify-center'>2024년 12월 14일 토요일 오전 11시</span>
+        <span className='flex items-center justify-center text-lg'>JK아트컨벤션 엠버루체홀</span>
       </div>
     </section>
     
