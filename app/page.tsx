cb940d3af0fbacd84ac61f98f4ad7d693577014d
Saemi Park <saemiprk@gmail.com>
2024-08-26 09:50:45 +0900

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import Account from "components/account";
 import DecCalendar from "components/decCalendar";
 import GestBook from "components/gestBook";
@@ -5,6 +6,16 @@ import ImageGallery from "components/imageGallery";
 import Invition from "components/invition";
 import Location from "components/location";
 import Main from "components/main";
+
+export const metadata: Metadata = {
+  title: "이태현, 박새미의 결혼식에 초대합니다.",
+  description: "2024년 12월 14일 오전 11시 JK아트컨벤션",
+  openGraph: {
+    images: {
+      url: '/images/main.jpg'
+    }
+  }
+};
 
 export default function Home() {
   return (
