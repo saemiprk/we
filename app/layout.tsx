ba09407735196d02e98b3c4f601533c6af08bb81
Saemi Park <saemiprk@gmail.com>
2024-08-26 13:28:55 +0900

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import RecoilProvider from "config/RecoilProvider";
 import ReactQueryClientProvider from "config/ReactClientProivider";
+import RecoilProvider from "config/RecoilProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
